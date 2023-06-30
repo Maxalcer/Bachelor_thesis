44416d39f677b67301c777b4d14419c6df776f3c
Maxalcer <m.alcer@gmx.de>
2023-06-30 13:48:24 +0200

    redo with douplet samples

--- netz/eval_netz.py
+++ netz/eval_netz.py
@@ -6,71 +6,71 @@ import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable 
 
-#netz = torch.load('saved_netz.py')
-#netzfc = torch.load('saved_fc_netz.py')
-#netzwon = torch.load('saved_fc_netz_won.py')
-netzfc = torch.load('saved_netz_join.py')
+netz = torch.load('saved_netz.py')
+netzfc = torch.load('saved_fc_netz.py')
+# netzwon = torch.load('saved_fc_netz_won.py')
+# netzfc = torch.load('saved_netz_join.py')
 
-#netz = netz.cpu()
+netz = netz.cpu()
 netzfc = netzfc.cpu() 
 #netzwon = netzwon.cpu()
 
 def test_var_noise():
-  #netz.eval()
+  netz.eval()
   netzfc.eval()
-  #hits = 0
+  hits = 0
   hits_fc = 0
   #hits_won = 0
-  #hits_algo = 0
+  hits_algo = 0
   for input, target in testing_data:
     input = torch.tensor(noise_matrix(np.array(input[0]), a, b))
-    #algo_erg = check_inf_sites(np.array(input))
-    #if (algo_erg == int(target[0])): hits_algo += 1
-    #expanded_input = torch.unsqueeze(input, 0)
-    #expanded_input = expanded_input.repeat(1,1,1,1)
-    #expanded_input = expanded_input.transpose(0, 1)
-    #expanded_input = Variable(expanded_input)
-    #output = netz(expanded_input)
+    algo_erg = check_inf_sites(np.array(input))
+    if (algo_erg == int(target[0])): hits_algo += 1
+    expanded_input = torch.unsqueeze(input, 0)
+    expanded_input = expanded_input.repeat(1,1,1,1)
+    expanded_input = expanded_input.transpose(0, 1)
+    expanded_input = Variable(expanded_input)
+    output = netz(expanded_input)
     output_fc = netzfc(input)
     #output_won = netzwon(input)
     target = Variable(target)
-    #if(torch.round(output) == target): hits += 1
+    if(torch.round(output) == target): hits += 1
     if(torch.round(output_fc) == target): hits_fc += 1
     #if(torch.round(output_won) == target): hits_won += 1
-  #return (hits/len(testing_data)), (hits_fc/len(testing_data)), (hits_won/len(testing_data)), (hits_algo/len(testing_data))
-  return (hits_fc/len(testing_data))
+  return (hits/len(testing_data)), (hits_fc/len(testing_data)), (hits_algo/len(testing_data))
+  #return (hits_fc/len(testing_data))
 
-#acc = []
+acc = []
 acc_fc = []
 #acc_won = []
-#acc_algo = []
+acc_algo = []
 noise = []
 
 b = 0
 
-testing_data = get_train_dataset("../data/test_join.txt", "../data/test_inf.txt", 1)
+testing_data = get_train_dataset("../data/test_fin_sorted.txt", "../data/test_inf_sorted.txt", 1)
 while (b < 0.51):
   print(b)
   if(b == 0): a = 0
   else: a = 10**(-5)
-  #net, fc, won, algo = test_var_noise()
-  fc = test_var_noise()
-  #acc.append(net)
+  net, fc, algo = test_var_noise()
+  # fc = test_var_noise()
+  acc.append(net)
   acc_fc.append(fc)
   #acc_won.append(won)
-  #acc_algo.append(algo)
+  acc_algo.append(algo)
   noise.append(b*100)
-  b += 0.01
+  b += 0.005
 
 
-#plt.plot(noise, acc, label = "CNN")
+plt.plot(noise, acc, label = "CNN")
 plt.plot(noise, acc_fc, label = "FCNN")
 #plt.plot(noise, acc_won, label = "FCNN without Noise")
-#plt.plot(noise, acc_algo, label = "Algorithm")
+plt.plot(noise, acc_algo, label = "Algorithm")
 plt.ylabel('Accuracy')
 plt.xlabel('Noise Level [%]')
 plt.title('Accuracy for different Noise Levels')
 plt.legend()
-plt.savefig('../results/Accuracy_Noise_Join.png')
+plt.savefig('../results/Accuracy_Noise.png')
 plt.show()
 
--- tests/generate_data_join.py
+++ tests/generate_data_join.py
@@ -5,8 +5,8 @@ import math
 import numpy as np
 from math import comb
 
-def generate_matrix():
-  process = subprocess.Popen([r'./ms', '10', '1', '-t', '5.0', '-s', '10'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=r'/home/max/msdir')
+def generate_matrix(nrows):
+  process = subprocess.Popen([r'./ms', str(nrows), '1', '-t', '5.0', '-s', '10'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=r'/home/max/msdir')
   result = process.communicate()
   lines = result[0].split('\n')
   seeds = [int(i) for i in lines[1].split(' ')]
@@ -14,33 +14,50 @@ def generate_matrix():
   del lines[len(lines)-1]
   matrix = [list(map(int, list(line))) for line in lines]
   return np.array(matrix), seeds
-  
-orig_data1 = []
-orig_data2 = []
+
+   
+probs = range(1,6)
+
+probs = [(1/math.exp(p)) for p in probs]
+
+probs = [(p/np.sum(probs)) for p in probs]
+
+orig_data = []
+
 train_data = []
+
+c = 0
+
+# new_row = [a ^ b for a, b in zip(matrix[row], matrix[row+10])]
 
 for i in range(1000):
   if(i % 5000 == 0): print(i/5000, "%")
-  
-  temp_data1 = generate_matrix()
-  temp_data2 = generate_matrix()
-  matrix1 = temp_data1[0].copy()
-  matrix2 = temp_data2[0].copy()
+  while True:    
+    n = np.random.choice(range(1,6), 1, p=probs)
+    n = n[0]
+    temp_data = generate_matrix(10+n)
+    temp_data += (n,)
+    matrix = temp_data[0].copy()
+    rows = np.random.choice(range(10), n, replace=False)
+    j = 0
+    for row in rows:
+      new_row = [a | b for a, b in zip(matrix[row], matrix[10+j])]
+      matrix[row] = new_row
+      j += 1 
+    matrix = np.delete(matrix, range(10,np.shape(matrix)[0]), 0)
 
-  for i in range(np.shape(matrix1)[0]):
-    for j in range(np.shape(matrix1)[1]):
-      matrix1[i,j] = matrix1[i,j] | matrix2[i,j]
-
-  train_data.append(matrix1)
-  orig_data1.append(temp_data1)
-  orig_data2.append(temp_data2)
+    if (np.any(np.delete(temp_data[0], range(10,np.shape(temp_data[0])[0]), 0) != matrix)):
+      train_data.append(matrix)
+      orig_data.append(temp_data)
+      break
 
 write_data("../data/test_join.txt", train_data)
 
-with open("../data/test_original_join1.txt", "w") as file:
-  for mat, seeds in orig_data1:
+with open("../data/test_original_join.txt", "w") as file:
+  for mat, seeds, n in orig_data:
     seedstr = [str(seed) for seed in seeds]
     file.write("seeds: "+ (' '.join(seedstr)) + "\n")
+    file.write("number of duplets: "+ str(n) + "\n")
     for line in mat:
       strl = ''
       for num in line:
@@ -48,16 +65,7 @@ with open("../data/test_original_join1.txt", "w") as file:
       file.write(strl+"\n")
     file.write("\n")
 
-with open("../data/test_original_join2.txt", "w") as file:
-  for mat, seeds in orig_data2:
-    seedstr = [str(seed) for seed in seeds]
-    file.write("seeds: "+ (' '.join(seedstr)) + "\n")
-    for line in mat:
-      strl = ''
-      for num in line:
-        strl += str(num)
-      file.write(strl+"\n")
-    file.write("\n")
+
 
 
 
