ea2897f8787c4b3c45d76f9a9317fe1fc78af32b
Maxalcer <m.alcer@gmx.de>
2023-07-12 09:40:34 +0200

    explainability noise

--- netz/explainability.py
+++ netz/explainability.py
@@ -21,30 +21,39 @@ def check_inf_sites_arg(m):
 
 netzfc = torch.load('saved_fc_netz.py')
 data = read_data_tens("../data/test_fin_sorted.txt")
+data_noise = read_data_tens("../data/test_fin_noise_15_sorted.txt")
 netzfc = netzfc.cpu() 
 netzfc.eval()
-
+print(netzfc(data[2]))
+print(netzfc(data_noise[2]))
 ig = IntegratedGradients(netzfc)
-#occ = Occlusion(netzfc)
-
-attr_ig = ig.attribute(data[4], target=0, n_steps=100)
-#attr_occ = occ.attribute(data[0], target=0, sliding_window_shapes=(2,))
+occ = Occlusion(netzfc)
+mat = data[2].unsqueeze(0)
+mat_noise = data_noise[2].unsqueeze(0)
+#attr_ig = ig.attribute(data[4], target=0, n_steps=100)
+attr_occ = occ.attribute(mat, sliding_window_shapes=(1,1))
+attr_occ_noise = occ.attribute(mat_noise, sliding_window_shapes=(1,1))
 alg = np.zeros((10,10))
-cols, rows = check_inf_sites_arg(data[4])
+cols, rows = check_inf_sites_arg(mat[0])
 for col in cols:
   for row in rows:
     alg[row, col] = -1
 
-plt.figure(figsize=(15,5))
-plt.subplot(1, 2, 1)
-plt.imshow(np.array(attr_ig), cmap='hot', interpolation='nearest')
+plt.figure(figsize=(20,5))
+plt.subplot(1, 3, 1)
+plt.imshow(np.array(attr_occ[0]), cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.title("Network")
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
+plt.imshow(np.array(attr_occ_noise[0]), cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.title("Network Noise")
+
+plt.subplot(1, 3, 3)
 plt.imshow(alg, cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.title("Algorithm")
 
-plt.savefig('../results/heatmap4.png')
+plt.savefig('../results/heatmap_noise.png')
 plt.show()
