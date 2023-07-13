0db50a0f3ed9091eaf6b923bc9bcf67b83562d5b
Maxalcer <m.alcer@gmx.de>
2023-07-13 16:40:28 +0200

    explainability values in heatmap, eval unsorted

--- netz/eval_netz.py
+++ netz/eval_netz.py
@@ -9,22 +9,20 @@ from torch.autograd import Variable
 #netz = torch.load('saved_conv_netz.py')
 netzfc = torch.load('saved_fc_netz.py', map_location=torch.device('cpu'))
 # netzwon = torch.load('saved_fc_netz_won.py')
-netzbig = torch.load('saved_fc_netz_12x12.py', map_location=torch.device('cpu'))
+netzuns = torch.load('saved_fc_netz_unsorted.py', map_location=torch.device('cpu'))
 
 def test_var_noise():
   #netz.eval()
   netzfc.eval()
-  netzbig.eval()
+  netzuns.eval()
   #hits = 0
   hits_fc = 0
-  hits_big = 0
+  hits_uns = 0
   #hits_won = 0
   hits_algo = 0
-  for (input, target), (input_big, target_big) in zip(testing_data, testing_data_big):
+  for input, target in testing_data:
     input = torch.tensor(noise_matrix(np.array(input[0]), a, b)).unsqueeze(0)
     input = sort_tensor(input)
-    input_big = torch.tensor(noise_matrix(np.array(input_big[0]), a, b)).unsqueeze(0)
-    input_big = sort_tensor(input_big)
     algo_erg = check_inf_sites(np.array(input[0]))
     if (algo_erg == int(target[0])): hits_algo += 1
     """
@@ -35,19 +33,19 @@ def test_var_noise():
     """
     # output = netz(expanded_input)
     output_fc = netzfc(input)
-    output_big = netzbig(input_big)
+    output_uns = netzuns(input)
     #output_won = netzwon(input)
     target = Variable(target)
     #if(torch.round(output) == target): hits += 1
     if(torch.round(output_fc) == target): hits_fc += 1
-    if(torch.round(output_big) == target_big): hits_big += 1
+    if(torch.round(output_uns) == target): hits_uns += 1
     #if(torch.round(output_won) == target): hits_won += 1
-  return (hits_big/len(testing_data)), (hits_fc/len(testing_data)), (hits_algo/len(testing_data))
+  return (hits_uns/len(testing_data)), (hits_fc/len(testing_data)), (hits_algo/len(testing_data))
   #return (hits_fc/len(testing_data))
 
 #acc = []
 acc_fc = []
-acc_big = []
+acc_uns = []
 #acc_won = []
 acc_algo = []
 noise = []
@@ -55,16 +53,16 @@ noise = []
 b = 0
 
 testing_data = get_train_dataset("../data/test_fin.txt", "../data/test_inf.txt", 1)
-testing_data_big = get_train_dataset("../data/test_fin_12x12.txt", "../data/test_inf_12x12.txt", 1)
+#testing_data_big = get_train_dataset("../data/test_fin_12x12.txt", "../data/test_inf_12x12.txt", 1)
 while (b <= 0.5):
   print(b)
   if(b == 0): a = 0
   else: a = 10**(-5)
-  big, fc, algo = test_var_noise()
+  uns, fc, algo = test_var_noise()
   #fc = test_var_noise()
   #acc.append(net)
   acc_fc.append(fc)
-  acc_big.append(big)
+  acc_uns.append(uns)
   #acc_won.append(won)
   acc_algo.append(algo)
   noise.append(b*100)
@@ -72,14 +70,14 @@ while (b <= 0.5):
 
 
 #plt.plot(noise, acc, label = "CNN")
-plt.plot(noise, acc_fc, label = "FCNN")
-plt.plot(noise, acc_big, label = "FCNN 12x12")
+plt.plot(noise, acc_uns, label = "FCNN")
+plt.plot(noise, acc_fc, label = "FCNN sorted")
 #plt.plot(noise, acc_won, label = "FCNN without Noise")
 plt.plot(noise, acc_algo, label = "Algorithm")
 plt.ylabel('Accuracy')
 plt.xlabel('Noise Level [%]')
 plt.title('Accuracy for different Noise Levels')
 plt.legend()
-plt.savefig('../results/Accuracy_Noise_12x12.png')
+plt.savefig('../results/Accuracy_Noise_unsorted.png')
 plt.show()
 
--- netz/explainability.py
+++ netz/explainability.py
@@ -19,41 +19,46 @@ def check_inf_sites_arg(m):
       if found_diff & found_same: return [i, j], (list(set(indices1) - set(indices2)) + list(set(indices2) - set(indices1)) + list(set(indices1) & set(indices2)))
   return []
 
-netzfc = torch.load('saved_fc_netz.py')
+netzfc = torch.load('saved_fc_netz_won.py')
 data = read_data_tens("../data/test_fin_sorted.txt")
-data_noise = read_data_tens("../data/test_fin_noise_15_sorted.txt")
+#data_noise = read_data_tens("../data/test_fin_noise_15_sorted.txt")
 netzfc = netzfc.cpu() 
 netzfc.eval()
-print(netzfc(data[2]))
-print(netzfc(data_noise[2]))
 ig = IntegratedGradients(netzfc)
 occ = Occlusion(netzfc)
 mat = data[2].unsqueeze(0)
-mat_noise = data_noise[2].unsqueeze(0)
-#attr_ig = ig.attribute(data[4], target=0, n_steps=100)
+#mat_noise = data_noise[2].unsqueeze(0)
+#attr_ig = ig.attribute(mat, target=0, n_steps=100)
 attr_occ = occ.attribute(mat, sliding_window_shapes=(1,1))
-attr_occ_noise = occ.attribute(mat_noise, sliding_window_shapes=(1,1))
+#attr_occ_noise = occ.attribute(mat_noise, sliding_window_shapes=(1,1))
 alg = np.zeros((10,10))
 cols, rows = check_inf_sites_arg(mat[0])
 for col in cols:
   for row in rows:
     alg[row, col] = -1
 
-plt.figure(figsize=(20,5))
-plt.subplot(1, 3, 1)
-plt.imshow(np.array(attr_occ[0]), cmap='hot', interpolation='nearest')
-plt.colorbar()
-plt.title("Network")
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15,5))
+heatmap_net = axs[0].imshow(np.array(attr_occ[0]), cmap='hot', interpolation='nearest')
+cbar_net = plt.colorbar(heatmap_net, ax=axs[0])
+axs[0].set_title("Network")
+heatmap_alg = axs[1].imshow(alg, cmap='hot', interpolation='nearest')
+cbar_alg = plt.colorbar(heatmap_alg, ax=axs[1])
+axs[1].set_title("Algorithm")
+mat = np.array(mat[0])
+for i in range(mat.shape[0]):
+    for j in range(mat.shape[1]):
+        text_color1 = 'white' if np.array(attr_occ[0])[i, j] < (-0.6) else 'black'
+        text_color2 = 'white' if alg[i, j] == (-1) else 'black'
+        text1 = axs[0].text(j, i, int(mat[i,j]), ha='center', va='center', color=text_color1)
+        text2 = axs[1].text(j, i, int(mat[i,j]), ha='center', va='center', color=text_color2)
 
+"""
 plt.subplot(1, 3, 2)
 plt.imshow(np.array(attr_occ_noise[0]), cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.title("Network Noise")
+"""
 
-plt.subplot(1, 3, 3)
-plt.imshow(alg, cmap='hot', interpolation='nearest')
-plt.colorbar()
-plt.title("Algorithm")
-
-plt.savefig('../results/heatmap_noise.png')
+plt.tight_layout()
+plt.savefig('../results/heatmap_won.png')
 plt.show()
